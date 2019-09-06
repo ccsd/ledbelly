8454b11be49ef3d1b9f9a08aa30e197333d77141
Robert Carroll <robert.c@nv.ccsd.net>
2019-09-06 13:58:42 -0700

    found live_module_item_updated.developer_key_id

--- src/lib/schemas/canvas.rb
+++ src/lib/schemas/canvas.rb
@@ -1624,6 +1624,7 @@ $schema = {
     context_role_meta: { type: 'string', size: 24 },
     context_sis_source_id: { type: 'string', size: 32 },
     context_type_meta: { type: 'string', size: 24 },
+    developer_key_id: { type: 'bigint' },
     event_name: { type: 'string', size: 19 },
     event_time: { type: 'datetime' },
     hostname: { type: 'string', size: 64 },
